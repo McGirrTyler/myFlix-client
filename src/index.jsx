268e9d3465ff7ef9bc4a0759c70d3d3ed46ae141
McGirrTyler <99841780+McGirrTyler@users.noreply.github.com>
2022-10-07 17:44:59 -0700

    Error Update
    In this commit, I attempted to fix error for react page not working. Code is fine to my knowledge, however localhost 1234 seems to not be responding.

--- src/index.jsx
+++ src/index.jsx
@@ -4,19 +4,19 @@ import ReactDOM from "react-dom";
 // Bundle
 import "./index.scss";
 
-//Main Component 
+//Main Component
 class MyFlixApplication extends React.Component {
-    render() {
-        return (
-            <div className="my-flix">
-            <div> Good morning </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="my-flix">
+        <div> Good morning </div>
+      </div>
+    );
+  }
 }
 
 //Finds Root of App
-const container = document.getElementsByClassName('app-container')[0];
+const container = document.getElementsByClassName("app-container")[0];
 
 //Render app in root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
