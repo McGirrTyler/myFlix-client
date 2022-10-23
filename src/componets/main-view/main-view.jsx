5ef0c9c76fcec5394d5ed03ca7c3bf281c088776
McGirrTyler <99841780+McGirrTyler@users.noreply.github.com>
2022-10-23 12:31:59 -0700

    Route Path
    In this commit, code for routing was added.

--- src/componets/main-view/main-view.jsx
+++ src/componets/main-view/main-view.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from "axios";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
 import { LoginView } from "../login-view/login-view";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
@@ -17,6 +19,21 @@ export class MainView extends React.Component {
     };
   }
 
+  getMovies(token) {
+    axios
+      .get("https://movieverseapi.herokuapp.com/movies", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        this.setState({
+          movies: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   componentDidMount() {
     axios
       .get("https://movieverseapi.herokuapp.com/movies")
@@ -30,52 +47,98 @@ export class MainView extends React.Component {
       });
   }
 
+  componentDidMount() {
+    let accessToken = localStorage.getItem("token");
+    if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem("user"),
+      });
+      this.getMovies(accessToken);
+    }
+  }
+
+  onLoggedIn(authData) {
+    console.log(authData);
+    this.setState({
+      user: authData.user.Username,
+    });
+
+    localStorage.setItem("token", authData.token);
+    localStorage.setItem("user", authData.user.Username);
+    this.getMovies(authData.token);
+  }
+
+  onLoggedOut() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    this.setState({
+      user: null,
+    });
+  }
+
   setSelectedMovie(newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie,
     });
   }
 
-  onLoggedIn(user) {
-    this.setState({
-      user,
-    });
-  }
-
   render() {
-    const { movies, selectedMovie, user } = this.state;
+    const { movies, user } = this.state;
 
     if (!user)
-      return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />; //LogiView
-
-    if (movies.length === 0) return <div className="main-view"></div>;
+      return (
+        <Row>
+          <Col>
+            <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+          </Col>
+        </Row>
+      );
+    if (movies.length === 0) return <div className="main-view" />;
 
     return (
-        <Container style={{ marginTop: 45 }}>
-          <Row className="main-view justify-content-md-center">
-            {selectedMovie ? (
-              <Col md={8}>
-                <MovieView
-                  movie={selectedMovie}
-                  onBackClick={(newSelectedMovie) => {
-                    this.setSelectedMovie(newSelectedMovie);
-                  }}
-                />
-              </Col>
-            ) : (
-              movies.map((movie) => (
-                <Col md={3} key={movie._id}>
-                  <MovieCard
-                    movie={movie}
-                    onMovieClick={(newSelectedMovie) => {
-                      this.setSelectedMovie(newSelectedMovie);
-                    }}
+      <Router>
+        <Row className="main-view justify-content-md-center">
+          <Route
+            exact
+            path="/"
+            render={() => {
+              if (!user)
+                return (
+                  <Col>
+                    <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+                  </Col>
+                );
+              return movies.map((m) => (
+                <Col md={3} key={m._id}>
+                  <MovieCard movie={m} />
+                </Col>
+              ));
+            }}
+          />
+          <Route
+            path="/register"
+            render={() => {
+              return (
+                <Col>
+                  <RegistrationView />
+                </Col>
+              );
+            }}
+          />
+          <Route
+            path="/movies/:movieId"
+            render={({ match }) => {
+              return (
+                <Col md={8}>
+                  <MovieView
+                    movie={movies.find((m) => m._id === match.params.movieId)}
                   />
                 </Col>
-              ))
-            )}
-          </Row>
-        </Container>
+              );
+            }}
+          />
+        </Row>
+      </Router>
     );
   }
 }
--- src/componets/registration-view/user-registration.jsx
+++ src/componets/registration-view/user-registration.jsx
@@ -1,39 +1,152 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
-import { Form, Button } from 'react-bootstrap';
+import { Row, Col, Button, Container, Form } from "react-bootstrap";
 
 export function RegistrationView(props) {
+  const [name, setName] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  //Hook
+  const [values, setValues] = useState({
+    nameErr: "",
+    usernameErr: "",
+    passwordErr: "",
+    emailErr: "",
+  });
+
+  //input validation
+  const validate = () => {
+    let isReq = true;
+    if (name) {
+      setValues({ ...values, nameErr: "Name is Required" });
+      isReq = false;
+    }
+    if (!username) {
+      setValues({ ...values, usernameErr: "Username is Required" });
+      isReq = false;
+    } else if (username.length < 5) {
+      setValues({
+        ...values,
+        usernameErr: "Username must be at least 5 characters long",
+      });
+      isReq = false;
+    }
+    if (!password) {
+      setValues({ ...values, passwordErr: "Password Required" });
+      isReq = false;
+    } else if (password.length < 6) {
+      setValues({
+        ...values,
+        passwordErr: "Password must be atleast 6 characters long",
+      });
+      isReq = false;
+    }
+    if (!email) {
+      setValues({ ...values, emailErr: "Email Required" });
+      isReq = false;
+    } else if (email.indexOf("@") === -1) {
+      setValues({ ...values, emailErr: "Email is invalid" });
+      isReq = false;
+    }
+
+    return isReq;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('https://movieverseapi.herokuapp.com/users', {
-      Username: username,
-      Password: password,
-      Email: email
-  })
-  .then(response => {
-    const data =response.data;
-    console.log(data);
-    window.open('/', '_self');
-    // _self makes page open within current page
-  })
-  .catch(err => {
-    console.log('Error registering the user');
-    alert('Something wasn\'t entered right.');
-  })
-};
+    const isReq = validate();
+    if (isReq) {
+      // Server Auth
+      axios
+        .post("https://movieverseapi.herokuapp.com/users", {
+          Name: name,
+          Username: username,
+          Password: password,
+          Email: email,
+        })
+        .then((response) => {
+          const data = response.data;
+          console.log(data);
+          alert("Registration succesful, please login!");
+          window.open("/", "_self");
+        })
+        .catch((response) => {
+          console.error(response);
+          alert("Unable to Register");
+        });
+    }
+  };
 
-return (
-   <Container>
-    <Row>
-      <Col>
-      Hello World
-      </Col>
-    </Row>
-   </Container>
-);
+  return (
+    <Container>
+      <Row className="mt-5">
+        <Col md={12}>
+          <Form>
+            <h3> Sign Up </h3>
+            <p></p>
+
+            <Form.Group controlId="formName" className="reg-form-inputs">
+              <Form.Label>Name:</Form.Label>
+              <Form.Control
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              {values.nameErr && <p>{values.nameErr}</p>}
+            </Form.Group>
+
+            <Form.Group controlId="formUsername" className="reg-form-inputs">
+              <Form.Label>Username:</Form.Label>
+              <Form.Control
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              {values.usernameErr && <p>{values.usernameErr}</p>}
+            </Form.Group>
+
+            <Form.Group controlId="formPassword" className="reg-form-inputs">
+              <Form.Label>Password:</Form.Label>
+              <Form.Control
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {values.passwordErr && <p>{values.passwordErr}</p>}
+            </Form.Group>
+
+            <Form.Group controlId="formEmail" className="reg-form-inputs">
+              <Form.Label>Email:</Form.Label>
+              <Form.Control
+                type="email"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              {values.usernameErr && <p>{values.usernameErr}</p>}
+            </Form.Group>
+
+            <Button variant="primary" type="submit" onClick={handleSubmit}>
+              Create Account
+            </Button>
+            <p></p>
+            <p>
+              {" "}
+              Already registered? <Link to={"/"}>Sign In</Link>Here
+            </p>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
+
+RegistrationView.propTypes = {
+  register: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Username: PropTypes.string.isRequired,
+    Password: PropTypes.string.isRequired,
+    Email: PropTypes.string.isRequired,
+  }),
+};
